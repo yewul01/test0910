6d607521ee3defd98087611593a22625b7846d35
yewul01 <68521830+yewul01@users.noreply.github.com>
2020-09-10 15:53:32 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -15,13 +15,6 @@
 
     $('#kimBox').load('main.html')
 
-    $('.topmenu > a').on('click',function(e) {
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#kimContainer')
-        $('#kimBox').load(url)
-    })
-
 
 
 
