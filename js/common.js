48ea1330087e0f0d92867ca08a412711c4b8930a
yewul01 <jjoham234@daum.net>
2020-09-10 15:52:25 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -15,7 +15,12 @@
 
     $('#kimBox').load('main.html')
 
-    $('.topmenu > a').
+    $('.topmenu > a').on('click',function(e) {
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#kimContainer')
+        $('#kimBox').load(url)
+    })
 
 
 
